1206efff030c478d421a49cb4769b9a7cc7d4fe6
masa-su <masa@weblab.t.u-tokyo.ac.jp>
2018-07-30 19:00:25 +0900

    fix a bug

--- Tars/models/__init__.py
+++ Tars/models/__init__.py
@@ -1,10 +1,11 @@
 from .vae import VAE
 from .vi import VI
 from .ml import ML
+from .regularization import Regularization
 
 __all__ = [
     'ML',
     'VAE',
     'VI',
-    'Regularization'
+    'Regularization',
 ]
