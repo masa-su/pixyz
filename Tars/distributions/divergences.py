fix the KL estimator

Tars/distributions/divergences.py
@@ -1,4 +1,5 @@
 import torch
+from torch.distributions.kl import kl_divergence
 
 from ..utils import get_dict_values
 
@@ -11,13 +12,13 @@ class KullbackLeibler(object):
         self.p1_name = self.p1.distribution_name
         self.p2_name = self.p2.distribution_name
 
-    def estimate(self, x):
+    def estimate(self, x, **kwargs):
         if self.p1_name == "Normal" and self.p2_name == "Normal":
             inputs = get_dict_values(x, self.p1.cond_var, True)
-            params1 = self.p1.get_params(**inputs)
+            params1 = self.p1.get_params(inputs, **kwargs)
 
             inputs = get_dict_values(x, self.p2.cond_var, True)
-            params2 = self.p2.get_params(**inputs)
+            params2 = self.p2.get_params(inputs, **kwargs)
 
             return gauss_gauss_kl(params1["loc"], params1["scale"],
                                   params2["loc"], params2["scale"])
@@ -27,7 +28,7 @@ class KullbackLeibler(object):
                                             self.p2_name))
 
 
-def gauss_gauss_kl(loc1, scale1, loc2, scale2):
+def gauss_gauss_kl(loc1, scale1, loc2, scale2, dim=1):
     _kl = torch.log(scale2) - torch.log(scale1) \
             + (scale1 + (loc1 - loc2)**2) / scale2 - 1
-    return 0.5 * torch.sum(_kl, dim=1)
+    return 0.5 * torch.sum(_kl, dim=dim)