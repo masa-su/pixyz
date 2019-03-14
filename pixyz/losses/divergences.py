25e6838548560f2f29743adc8eecea7b263152a9
nnknk0802 <yatsugatake_nnk@yahoo.co.jp>
2019-03-14 15:20:00 +0900

    modified divergences

--- pixyz/losses/divergences.py
+++ pixyz/losses/divergences.py
@@ -30,6 +30,15 @@ class KullbackLeibler(Loss):
             return gauss_gauss_kl(params1["loc"], params1["scale"],
                                   params2["loc"], params2["scale"]), x
 
+        if (self._p1.distribution_name == "vonMisesFisher" and \
+            self._p2.distribution_name == "HypersphericalUniform"):
+            inputs = get_dict_values(x, self._p1.input_var, True)
+            params1 = self._p1.get_params(inputs, **kwargs)
+            
+            hyu_dim = self._p2.dim
+            return vmf_hyu_kl(params1["loc"], params1["scale"],
+                              hyu_dim, self.device), x
+        
         raise Exception("You cannot use these distributions, "
                         "got %s and %s." % (self._p1.distribution_name,
                                             self._p2.distribution_name))
@@ -48,3 +57,15 @@ def gauss_gauss_kl(loc1, scale1, loc2, scale2, dim=None):
     dim_list = list(torch.arange(_kl.dim()))
     _kl = torch.sum(_kl, dim=dim_list[1:])
     return _kl
+
+def vmf_hyu_kl(vmf_loc, vmf_scale, hyu_dim, device):
+    __m = vmf_loc.shape[-1]
+    vmf_entropy = vmf_scale * ive(__m/2, vmf_scale) / ive((__m/2)-1, vmf_scale)
+    vmf_log_norm = ((__m / 2 - 1) * torch.log(vmf_scale) - (__m / 2) * math.log(2 * math.pi) - (
+        vmf_scale + torch.log(ive(__m / 2 - 1, vmf_scale))))
+    vmf_log_norm = vmf_log_norm.view(*(vmf_log_norm.shape[:-1]))
+    vmf_entropy = vmf_entropy.view(*(vmf_entropy.shape[:-1])) + vmf_log_norm
+
+    hyu_entropy = math.log(2) + ((hyu_dim + 1) / 2) * math.log(math.pi) - torch.lgamma(
+        torch.Tensor([(hyu_dim + 1) / 2])).to(device)
+    return - vmf_entropy + hyu_entropy
