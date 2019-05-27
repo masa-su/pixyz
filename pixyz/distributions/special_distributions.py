043730fa539891b81a14f0d4690c1a4a5ed86618
masa-su <masa@weblab.t.u-tokyo.ac.jp>
2019-05-27 23:44:55 +0900

    add docs in Deterministic and DataDistribution

--- pixyz/distributions/special_distributions.py
+++ pixyz/distributions/special_distributions.py
@@ -7,6 +7,31 @@ from ..utils import get_dict_values
 class Deterministic(Distribution):
     """
     Deterministic distribution (or degeneration distribution)
+
+    Examples
+    --------
+    >>> import torch
+    >>> class Generator(Deterministic):
+    ...     def __init__(self):
+    ...         super().__init__(cond_var=["z"], var=["x"])
+    ...         self.model = torch.nn.Linear(64, 512)
+    ...     def forward(self, z):
+    ...         return {"x": self.model(z)}
+    >>> p = Generator()
+    >>> print(p)
+    Distribution:
+      p(x|z)
+    Network architecture:
+      Generator(
+        name=p, distribution_name=Deterministic,
+        var=['x'], cond_var=['z'], input_var=['z'], features_shape=torch.Size([])
+        (model): Linear(in_features=64, out_features=512, bias=True)
+      )
+    >>> sample = p.sample({"z": torch.randn(1, 64)})
+    >>> p.log_prob().eval(sample) # log_prob is not defined.
+    Traceback (most recent call last):
+     ...
+    NotImplementedError
     """
 
     def __init__(self, **kwargs):
@@ -37,7 +62,22 @@ class Deterministic(Distribution):
 class DataDistribution(Distribution):
     """
     Data distribution.
-    TODO: Fix this behavior if multiplied with other distributions
+
+    Samples from this distribution equal given inputs.
+
+    Examples
+    --------
+    >>> import torch
+    >>> p = DataDistribution(var=["x"])
+    >>> print(p)
+    Distribution:
+      p_{data}(x)
+    Network architecture:
+      DataDistribution(
+        name=p_{data}, distribution_name=Data distribution,
+        var=['x'], cond_var=[], input_var=['x'], features_shape=torch.Size([])
+      )
+    >>> sample = p.sample({"x": torch.randn(1, 64)})
     """
 
     def __init__(self, var, name="p_{data}"):
