74ef413fab84a346d8cfa519a3121d8d7d935f9d
kaneko <kaneko@weblab.t.u-tokyo.ac.jp>
2019-12-24 16:43:01 +0900

    start to draw multi-graph

--- pixyz/utils.py
+++ pixyz/utils.py
@@ -273,7 +273,7 @@ def convert_latex_name(name):
     return sympy.latex(sympy.Symbol(name))
 
 
-def print_pgm(dist, filename):
+def print_pgm(dist, filename, enc_dist=None):
     """
     plot graph of probablistic graphical model of Distribution.
 
@@ -281,6 +281,7 @@ def print_pgm(dist, filename):
     ----------
     dist: Distribution
     filename: str
+    enc_dists: list of Distribution
 
     Examples
     --------
@@ -290,8 +291,9 @@ def print_pgm(dist, filename):
     default_node_size = 1
     default_margin = 1
 
-    glgraph = glg.Graph()
-    _dist2graph(dist, glgraph, default_node_size)
+    glgraph, var_dict = _dist2graph(dist, default_node_size, forward=True)
+    if enc_dist is not None:
+        _dist2graph(enc_dist, default_node_size, forward=False, glgraph=glgraph, var_dict=var_dict)
 
     layout = _GeneralSugiyamaLayout(glgraph, default_node_size, default_margin)
     layout.draw()
@@ -299,20 +301,22 @@ def print_pgm(dist, filename):
     _draw_graph(glgraph, filename, default_node_size)
 
 
-def _dist2graph(dist, glgraph: glg.Graph, node_size, var_dict=None):
+def _dist2graph(dist, node_size, forward=True, glgraph: glg.Graph = None, var_dict=None):
     if var_dict is None:
         var_dict = {}
+    if glgraph is None:
+        glgraph = glg.Graph()
     if isinstance(dist, pixyz.distributions.distributions.MultiplyDistribution):
-        _dist2graph(dist._parent, glgraph, node_size, var_dict)
-        _dist2graph(dist._child, glgraph, node_size, var_dict)
+        _dist2graph(dist._parent, node_size, forward, glgraph, var_dict)
+        _dist2graph(dist._child, node_size, forward, glgraph, var_dict)
     elif isinstance(dist, pixyz.distributions.distributions.MarginalizeVarDistribution):
         # marginalized var is not observed.
-        _dist2graph(dist.p, glgraph, node_size, var_dict)
+        _dist2graph(dist.p, node_size, forward, glgraph, var_dict)
         for var_name in dist._marginalize_list:
             var_dict[var_name].data = _PGMNode(var_name, node_size, observed=False)
     elif isinstance(dist, pixyz.distributions.distributions.ReplaceVarDistribution):
-        _dist2graph(dist.p, glgraph, node_size, var_dict)
-        _replace_var_name(glgraph, var_dict, dist._replace_dict)
+        _dist2graph(dist.p, node_size, forward, glgraph, var_dict)
+        _replace_var_name(glgraph, var_dict, dist._replace_dict, forward)
     else:
         var = dist.var
         for var_name in var:
@@ -323,10 +327,15 @@ def _dist2graph(dist, glgraph: glg.Graph, node_size, var_dict=None):
                 if cond_var_name not in var_dict:
                     var_dict[cond_var_name] = glg.Vertex(_PGMNode(cond_var_name, node_size))
                 # the direction of the edge is inversed because root nodes are aligned with the baseline.
-                glgraph.add_edge(glg.Edge(var_dict[cond_var_name], var_dict[var_name]))
+                to_node = var_dict[cond_var_name]
+                from_node = var_dict[var_name]
+                if not forward:
+                    from_node, to_node = (to_node, from_node)
+                glgraph.add_edge(glg.Edge(from_node, to_node, data=_PGMEdge(dotted=not forward)))
+    return glgraph, var_dict
 
 
-def _replace_var_name(glgraph, var_dict, replace_pair_dict):
+def _replace_var_name(glgraph, var_dict, replace_pair_dict, forward=True):
     for key, replaced_key in replace_pair_dict.items():
         if key not in var_dict:
             raise ValueError(f"replaced_pair_dict has a unknown source key {key}.")
@@ -336,8 +345,13 @@ def _replace_var_name(glgraph, var_dict, replace_pair_dict):
         value = var_dict[key]
         renamed_value = glg.Vertex(_PGMNode(replaced_key, value.data.node_size, observed=value.data.observed))
         # replace edges
-        renamed_edges_in = [glg.Edge(edge.v[0], renamed_value) for edge in value.e_in()]
-        renamed_edges_out = [glg.Edge(renamed_value, edge.v[1]) for edge in value.e_out()]
+        renamed_edges_in = [glg.Edge(edge.v[0] if forward else renamed_value,
+                                     renamed_value if forward else edge.v[0],
+                                     data=_PGMEdge(dotted=not forward)) for edge in value.e_in()]
+        renamed_edges_out = [glg.Edge(renamed_value if forward else edge.v[1],
+                                      edge.v[1] if forward else renamed_value,
+                                      data=_PGMEdge(dotted=not forward)) for edge in value.e_out()]
+
         var_dict[replaced_key] = renamed_value
         glgraph.add_vertex(renamed_value)
         for edge in renamed_edges_in + renamed_edges_out:
@@ -383,6 +397,26 @@ class _GeneralSugiyamaLayout:
             top_left = [top_left[0] + xyrange[1] - xyrange[0] + self.margin, top_left[1]]
 
 
+def _glf_edge_count(graph):
+    from_to_dict = {}
+    for e in graph.E():
+        if e.v in from_to_dict:
+            from_to_dict[e.v].append(e)
+        elif (e.v[1], e.v[0]) in from_to_dict:
+            from_to_dict[(e.v[1], e.v[0])].append(e)
+        else:
+            from_to_dict[e.v] = [e]
+    result = {}
+    for e in graph.E():
+        if e.v in from_to_dict:
+            result[e] = len(from_to_dict[e.v])
+        elif (e.v[1], e.v[0]) in from_to_dict:
+            result[e] = len(from_to_dict[(e.v[1], e.v[0])])
+        else:
+            raise ValueError()
+    return result
+
+
 def _draw_graph(graph, filename, node_size, transparent=False):
     fig = plt.figure(frameon=not transparent)
     xyrange = _get_vertex_range(graph.V())
@@ -391,17 +425,40 @@ def _draw_graph(graph, filename, node_size, transparent=False):
     pyplot_margin = linewidth / 40
     ax.set_xlim(xyrange[0] - pyplot_margin, xyrange[1] + pyplot_margin)
     ax.set_ylim(xyrange[2] - pyplot_margin, xyrange[3] + pyplot_margin)
-    ax.axis('off')
+    # ax.axis('off')
 
-    for e in graph.E():
-        _PGMEdge(e).draw(ax, node_size)
+    ec = _glf_edge_count(graph)
 
     for v in graph.V():
-        v.data.draw(ax, v)
+        v.data.draw(v, ax, fig)
+
+    for e in graph.E():
+        e.data.straight = (ec[e] == 1)
+        e.data.draw(e, ax, fig, node_size)
 
     ax.set_facecolor('w')
     with open(filename, 'wb') as f:
         fig.canvas.print_png(f)
+
+
+def _convert_data2point_scale(ax, fig, target):
+    # constant value of matplotlib
+    ppi = 72
+    # ax_length = ax.bbox.get_points()[1][0] -ax.bbox.get_points()[0][0]
+    # ax_point = ax_length*ppi/fig.dpi
+    # result = ax_point/x_size
+    # TODO: スケールが謎
+    tf = ax.transData.transform([1, 0]) * (ppi / fig.dpi)
+    return target * (tf[0] - tf[1])
+
+
+def _convert_point2data_scale(ax, fig, target):
+    # constant value of matplotlib
+    ppi = 72
+    # ax_length = ax.bbox.get_points()[1][0] -ax.bbox.get_points()[0][0]
+    # ax_point = ax_length*ppi/fig.dpi
+    # result = ax_point/x_size
+    return ax.transData.invereted().transform(target) / (ppi / fig.dpi)
 
 
 class _PGMNode:
@@ -411,10 +468,10 @@ class _PGMNode:
         self.node_size = node_size
         # magic scaling number for matplotlib.patches
         self.radius = node_size / 2
-        self.linewidth = node_size * 4
+        self.linewidth = node_size * 2
         self.fontsize = node_size * 30
 
-    def draw(self, ax, g_vertex):
+    def draw(self, g_vertex, ax, fig):
         fc = 'gray' if self.observed else 'w'
         circle = patches.Circle(xy=g_vertex.view.xy, radius=self.radius, fc=fc, ec='k', linewidth=self.linewidth)
         ax.add_patch(circle)
@@ -423,27 +480,42 @@ class _PGMNode:
 
 
 class _PGMEdge:
-    def __init__(self, edge):
-        self.edge = edge
-        self.linestyle = '-'
-        self.rad = 0
+    def __init__(self, straight=False, dotted=True):
+        self.linestyle = '-' if not dotted else '--'
+        self.straight = straight
+        self.forward = not dotted
 
-    def draw(self, ax, node_size):
+    def draw(self, glf_edge, ax, fig, node_size):
         # magic scaling number for matplotlib.patches
-        linewidth = node_size * 4
+        linewidth = node_size * 2
         # path = patches.Path([e.v[0].view.xy, e.v[1].view.xy])
-        node0, node1 = self.edge.v
-        self.add_arrow(ax, node0.view.xy, node1.view.xy, node_size * 33, linewidth, self.linestyle, self.rad)
+        node0, node1 = glf_edge.v
+        if not self.forward:
+            node0, node1 = (node1, node0)
+        self.add_arrow(ax, node0.view.xy, node1.view.xy, _convert_data2point_scale(ax, fig, node_size/2),
+                       linewidth, self.linestyle,
+                       node_size * 0.3, node_size * 1.3, 0 if self.straight else 0.3)
 
     @staticmethod
-    def add_arrow(ax, v1, v2, shrink, linewidth, linestyle, rad, path=None):
+    def add_arrow(ax, v1, v2, shrink, linewidth, linestyle, head_width, head_length, rad, path=None):
+        # ax.add_patch(patches.FancyArrowPatch(v1, v2,
+        #                                      arrowstyle=patches.ArrowStyle("-|>", head_length=head_length,
+        #                                                                    head_width=head_width),
+        #                                      arrow_transmuter=None,
+        #                                      connectionstyle=patches.ConnectionStyle("arc3", rad=rad), connector=None,
+        #                                      patchA=None, patchB=None, shrinkA=shrink, shrinkB=shrink,
+        #                                      mutation_scale=1, mutation_aspect=None, dpi_cor=1,
+        #                                      color='k', path=path, capstyle='butt',
+        #                                      linewidth=linewidth, linestyle=linestyle,
+        #                                      ))
         ax.annotate('', xy=v1, xycoords='data', xytext=v2, textcoords='data',
-                    arrowprops=dict(arrowstyle='-|>', color='k',
-                                    path=path,
+                    arrowprops=dict(arrowstyle=
+                                    patches.ArrowStyle("-|>", head_length=head_length, head_width=head_width),
+                                    color='k', path=path,
                                     shrinkA=shrink, shrinkB=shrink,
-                                    linewidth=linewidth,
-                                    linestyle=linestyle,
-                                    connectionstyle=f"arc3,rad={rad}"))
+                                    linewidth=linewidth, linestyle=linestyle,
+                                    capstyle='butt',
+                                    connectionstyle=patches.ConnectionStyle("arc3", rad=rad)))
 
 
 def _get_vertex_range(vertices):
@@ -462,8 +534,20 @@ def _prepare_dist():
     x_dim = 20
     y_dim = 30
     a_dim = 50
-    long_name = "too_long_name"
+    long_name = r"\phi_{x,y}"
 
+    # b -> a
+    class P0(Normal):
+        def __init__(self):
+            super(P0, self).__init__(cond_var=["b"], var=["a"], name="p_{0}")
+
+            self.fc21 = nn.Linear(10, a_dim)
+            self.fc22 = nn.Linear(10, a_dim)
+
+        def forward(self, b):
+            return {"loc": self.fc21(b), "scale": F.softplus(self.fc22(b))}
+
+    # (y,a) -> x=phi
     class P1(Normal):
         def __init__(self):
             super(P1, self).__init__(cond_var=["y", "a"], var=["x"], name="p_{1}")
@@ -479,6 +563,7 @@ def _prepare_dist():
             h12 = torch.cat([h1, h2], 1)
             return {"loc": self.fc21(h12), "scale": F.softplus(self.fc22(h12))}
 
+    # (phi, y) -> z
     class P2(Normal):
         def __init__(self):
             super(P2, self).__init__(cond_var=[long_name, "y"], var=["z"], name="p_{2}")
@@ -493,11 +578,25 @@ def _prepare_dist():
             h4 = F.relu(self.fc4(torch.cat([h3, y], 1)))
             return {"loc": self.fc51(h4), "scale": F.softplus(self.fc52(h4))}
 
+    # z -> y
+    class Q1(Normal):
+        def __init__(self):
+            super(Q1, self).__init__(cond_var=["z"], var=["y"], name="q_{1}")
+
+            self.fc1 = nn.Linear(20, 10)
+            self.fc21 = nn.Linear(10, y_dim)
+            self.fc22 = nn.Linear(10, y_dim)
+
+        def forward(self, a, z):
+            h1 = F.relu(self.fc1(z))
+            return {"loc": self.fc21(h1), "scale": F.softplus(self.fc22(h1))}
+
     p4 = Normal(loc=torch.tensor(0.), scale=torch.tensor(1.), var=["a"], features_shape=[a_dim], name="p_{4}")
     p6 = Normal(loc=torch.tensor(0.), scale=torch.tensor(1.), var=["y"], features_shape=[y_dim], name="p_{6}")
     p7 = Normal(loc=torch.tensor(0.), scale=torch.tensor(1.), var=["w"], features_shape=[y_dim], name="p_{7}")
     p8 = ReplaceVarDistribution(p7, {'w': 'W'})
 
+    p0 = P0()
     p1 = P1()
     p1_r = ReplaceVarDistribution(p1, {'x': long_name})
     p2 = P2()
@@ -506,14 +605,18 @@ def _prepare_dist():
     p5 = p3 * p4
     p5.name = "p_{5}"
     p_all = p1_r * p2 * p4 * p6 * p8
+    p_all2 = p0 * p1_r * p2 * p6 * p8
     p_all.name = "p_{all}"
     p_marg = MarginalizeVarDistribution(p_all, ['z'])
-    return p_marg
+    p_marg2 = MarginalizeVarDistribution(p_all2, ['z'])
+    q1 = Q1()
+    return p_marg, q1, p_marg2
 
 
 def _test_print_pgm():
-    dist = _prepare_dist()
-    print_pgm(dist, "tmp_daft.png")
+    dist, enc_dist, dist2 = _prepare_dist()
+    print_pgm(dist, "tmp_daft.png", enc_dist=enc_dist)
+    print_pgm(dist2, "tmp_daft2.png", enc_dist=enc_dist)
 
 
 if __name__ == "__main__":
